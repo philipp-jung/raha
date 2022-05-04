979109321e148bc11ead1eebd9ad941820d2e809
Philipp Jung <philipp.jung@verdane.com>
2022-05-04 09:56:05 +0200

    transform renuver datasets

--- datasets/renuver/cars/transform-csv.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-for root, _, files in os.walk('./'):
-    for file in files:
-        end = file.split('.')[-1]
-        if end == 'csv':
-            pd.read_csv(root+file, sep=';').to_csv(root+file)
---
+++ datasets/renuver/transform-csv.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+# small script to transform .csv files with semicolon-separated values
+# into .csv files with comma-separated values. Just move the script in
+# a directory containing ;-separted .csv files and run it. Will overwrite
+# existing .csv files.
+
+for root, _, files in os.walk('./'):
+    for file in files:
+        end = file.split('.')[-1]
+        if end == 'csv':
+            pd.read_csv(root+file, sep=';').to_csv(root+file)
