ea159a9b2f4fbae64947be21d7a3a12c783227b3
Philipp Jung <philippjung@posteo.de>
2022-06-26 23:43:39 +0200

    prepare experiment old datasets

--- raha/run_experiment.py
+++ raha/run_experiment.py
@@ -1,6 +1,37 @@
 import raha
 from ruska import Ruska
 
+def run_og_baran(c: dict):
+    data_dict = {
+        "name": c["dataset"],
+        "path": f"../datasets/{c['dataset']}/dirty.csv",
+        "clean_path": f"../datasets/{c['dataset']}/clean.csv",
+    }
+
+    data = raha.Dataset(data_dict, n_rows=c["n_rows"])
+    data.detected_cells = dict(data.get_actual_errors_dictionary())
+    app = raha.Correction()
+    app.LABELING_BUDGET = c["labeling_budget"]
+    app.VERBOSE = False
+    app.FEATURE_GENERATORS = c["feature_generators"]
+    app.CLASSIFICATION_MODEL = c["classification_model"]
+    app.VICINITY_ORDERS = c["vicinity_orders"]
+    app.VICINITY_FEATURE_GENERATOR = c["vicinity_feature_generator"]
+    app.N_BEST_PDEPS = c["n_best_pdeps"]
+    app.PDEP_SCORE_STRATEGY = c["score_strategy"]
+    app.EXCLUDE_VALUE_SPECIAL_CASE = c["exclude_value_special_case"]
+
+    d = app.initialize_dataset(data)
+    app.initialize_models(d)
+    while len(d.labeled_tuples) < app.LABELING_BUDGET:
+        app.sample_tuple(d, random_seed=None)
+        app.label_with_ground_truth(d)
+        app.update_models(d)
+        app.generate_features(d, synchronous=True)
+        app.predict_corrections(d)
+
+        p, r, f = d.get_data_cleaning_evaluation(d.corrected_cells)[-3:]
+    return {"result": {"precision": p, "recall": r, "f1": f}, "config": c}
 
 def run_baran(c: dict):
     rate_formatted = str(c['error_fraction']).split(".")[1]
@@ -79,7 +110,7 @@ if __name__ == "__main__":
         name="2022-06-26-ignore-value-model",
         description="""Ich ignoriere die Vorschläge des Value-Modells bei
         Imputation-Problemen. Ich möchte untersuchen, ob das die Reinigung
-        des RENUVER-Datensatzes verbessert.""",
+        auf den alten Datensätzen kaputt macht.""",
         commit="füge ich später ein :)",
         config={
             "dataset": "breast-cancer",
@@ -96,19 +127,19 @@ if __name__ == "__main__":
         },
         ranges={
             "error_fraction": [0.01, 0.02, 0.03, 0.04, 0.05],
-            "dataset": ['cars', 'bridges', 'glass', 'physician', 'resturant'],
+            "dataset": ['cars', 'bridges', 'glass', 'restaurant'],
         },
         runs=5,
         save_path="/root/measurements/",
     )
 
-    rsk.run(experiment=run_baran_renuver, parallel=True)
+    rsk.run(experiment=run_og_baran, parallel=True)
 
     rsk_naive = Ruska(
         name="2022-06-26-ignore-value-model-naive",
         description="""Ich ignoriere die Vorschläge des Value-Modells bei
         Imputation-Problemen. Ich möchte untersuchen, ob das die Reinigung
-        des RENUVER-Datensatzes verbessert.""",
+        auf den alten Datensätzen kaputt macht.""",
         commit="füge ich später ein :)",
         config={
             "dataset": "breast-cancer",
@@ -126,10 +157,10 @@ if __name__ == "__main__":
         },
         ranges={
             "error_fraction": [0.01, 0.02, 0.03, 0.04, 0.05],
-            "dataset": ['cars', 'bridges', 'glass', 'physician', 'resturant'],
+            "dataset": ['cars', 'bridges', 'glass', 'restaurant'],
         },
         runs=5,
         save_path="/root/measurements/",
     )
 
-    rsk_naive.run(experiment=run_baran_renuver, parallel=True)
+    rsk_naive.run(experiment=run_og_baran, parallel=True)
