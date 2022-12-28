177c3f691a1dc08ae099562cece2ac9b3c879fae
Philipp Jung <philipp.jung@verdane.com>
2022-12-28 16:55:25 +0100

    update validate-raha with new ruska setup

--- raha/run_experiment.py
+++ raha/run_experiment.py
@@ -1,13 +1,23 @@
+import os
+from pathlib import Path
+import logging
 import helpers, dataset, correction
 from ruska import Ruska
 
+import dotenv
+dotenv.load_dotenv()
 
-def run_baran(c: dict):
+
+def run_baran(i: int, c: dict):
     """
     Wrapper for Ruska to run parameterized Cleaning Experiments.
     @param c: a dictionary that contains all parameters the Cleaning Experiment requires.
     @return: A dictionary containing measurements and the configuration of the measurement.
     """
+    logger = logging.getLogger('ruska')
+    logger.info(f'Started experiment {i}.')
+    logger.debug(f'Started experiment {i} with config {c}.')
+
     version = c['run'] + 1  # dataset versions are 1-indexed, Ruska runs are 0-indexed.
     data_dict = helpers.get_data_dict(c['dataset'], c['error_fraction'], version, c['error_class'])
 
@@ -16,27 +26,54 @@ def run_baran(c: dict):
 
     app = correction.Correction()
     app.VERBOSE = False
-    correction_dictionary = app.run(data)
-    p, r, f = data.get_data_cleaning_evaluation(correction_dictionary)[-3:]
+    p, r, f = app.run(data)
+    logger.info(f'Finished experiment {i}.')
     return {"result": {"precision": p, "recall": r, "f1": f}, "config": c}
 
 
 if __name__ == "__main__":
+    experiment_name = "2022-12-25-openml-validate-raha"
+    # save_path = "/root/measurements/"
+    save_path="/Users/philipp/code/validate_raha/raha"
+
+    logging.root.handlers = []  # deletes the default StreamHandler to stderr.
+    logging.getLogger("ruska").setLevel(logging.DEBUG)
+
+    # create formatter to use with the handlers
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    # create console handler with a higher log level to reduce noise
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    ch.setFormatter(formatter)
+    logging.root.addHandler(ch)
+
+    fh = logging.FileHandler(str(Path(save_path)/Path(experiment_name))+'.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logging.root.addHandler(fh)
+
     rsk_openml = Ruska(
-        name="2022-12-13-openml",
+        name=experiment_name,
+        save_path=save_path,
         description="Measure all openml datasets to benchmark with Baran.",
         commit="",
         config={
-            "dataset": "letter",
+            "dataset": "beers",
             "error_class": "simple_mcar",
             "error_fraction": 1,
         },
         ranges={
-            "dataset": [137, 1481, 184, 41027, 4135, 42493, 6],
-            "error_fraction": [1, 5, 10]
+            "dataset": [137, 1481, 184, 41027, 42493, 6],
+            "error_fraction": [1, 5, 10],
+            "error_class": ["imputer_simple_mcar", "simple_mcar"]
         },
         runs=3,
-        save_path="/root/measurements/",
+        chat_id=os.environ['TELEGRAM_CHAT_ID'],
+        token=os.environ['TELEGRAM_BOT_TOKEN'],
     )
 
     rsk_openml.run(experiment=run_baran, parallel=False)
