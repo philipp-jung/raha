f7315e39853eb72945bc0a464afaec937318ab46
Philipp Jung <philipp.jung@verdane.com>
2023-04-12 22:31:02 +0200

    add update_models

--- raha/correction.py
+++ raha/correction.py
@@ -841,7 +841,7 @@ class Correction:
         while len(d.labeled_tuples) < self.LABELING_BUDGET:
             self.sample_tuple(d, random_seed=random_seed)
             self.label_with_ground_truth(d)
-            # self.update_models(d)
+            self.update_models(d)
             self.prepare_augmented_models(d)
             self.generate_features(d, synchronous=True)
             self.generate_synth_features(d, synchronous=True)
